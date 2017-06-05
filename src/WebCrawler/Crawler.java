fe992daa02f11830edc58991415ef3745b325f7e
BenGiese22 <bengiese22@gmail.com>
2017-06-04 22:20:30 -0500

    Slightly Update Version 6/4/2017

--- src/WebCrawler/Crawler.java
+++ src/WebCrawler/Crawler.java
@@ -6,7 +6,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 /**
  * Created by bengi on 5/29/2017.
@@ -31,11 +36,18 @@ public class Crawler {
             e.printStackTrace();
         }
 
-        System.out.println(getTitle());
-        printTotalLinks();
-        printTotalImages();
-        //System.out.println(title + "\n");
+        System.out.println(parseDoc(nfdlWikiSite));
+
+        try {
+            System.out.println(onlyParagraphs(getURLSource(nfdlWikiSite)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(doc.select("<p>"));
+        //System.out.println(doc.title() + "\n");
         //System.out.println(doc.toString());
+
     }
 
     private static void loadWebpage(String url) {
@@ -52,7 +64,7 @@ public class Crawler {
 
     private static void printTotalLinks() {
         Elements links = totalLinks();
-        for(Element link : links) {
+        for (Element link : links) {
             System.out.println("\nlink : " + link.attr("href"));
             System.out.println("text : " + link.text());
         }
@@ -72,7 +84,7 @@ public class Crawler {
 
     private static void printTotalImages() {
         Elements images = totalImages();
-        for(Element image : images) {
+        for (Element image : images) {
             System.out.println("\nsrc : " + image.attr("src"));
             System.out.println("height : " + image.attr("height"));
             System.out.println("width : " + image.attr("width"));
@@ -88,4 +100,34 @@ public class Crawler {
         return doc.body().text();
     }
 
+    private static String getWebsiteURL() {
+        return doc.location();
+    }
+
+    private static String getURLSource(String url) throws IOException {
+        URL tempURL = new URL(nfdlWikiSite);
+        URLConnection tempC = tempURL.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(tempC.getInputStream(),"UTF-8"));
+        String inputLine;
+        StringBuilder a = new StringBuilder();
+        while((inputLine = in.readLine()) !=null) {
+            a.append(inputLine + "\n");
+
+        }
+        in.close();
+        return a.toString();
+    }
+
+    private static String onlyParagraphs(String sourceCode) {
+        Document tempDoc = Jsoup.parse(sourceCode);
+        StringBuilder a = new StringBuilder();
+        for( Element element : tempDoc.select("p")) {
+            a.append(element.text() + "\n");
+        }
+        return a.toString();
+    }
+
+    private static String parseDoc(String sourceCode) {
+        return Jsoup.parse(sourceCode).text();
+    }
 }
